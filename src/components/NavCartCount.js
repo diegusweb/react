aeb9b894ab156b0476e6063006361fc377bf2b18
Diego Rueda <diego.rueda2@gmail.com>
2018-07-09 23:08:22 -0400

    update files header

--- src/components/NavCartCount.js
+++ src/components/NavCartCount.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { getCarts } from "../utils/api";
+import logo from '../images/indice.png';
 import { refcar } from "./config/constants";
 
 class NavCartCount extends Component {
 
   render() {
-    return <div className="cart">{refcar.length} items in the cart</div>;
+    return <div className="cart">{refcar.length} items in the cart <img src={logo} width="100" height="50" /> </div> ;
   }
 }
 
